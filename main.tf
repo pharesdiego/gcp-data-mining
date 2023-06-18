beea96423d37107e53d90917655833b2f7e21bb3
Diego Perez <pharesdiego@gmail.com>
2023-06-17 22:16:57 -0500

    import transform_marketcap cloud function into terraform

--- main.tf
+++ main.tf
@@ -32,20 +32,49 @@ resource "google_storage_bucket" "cloud_fns_bucket" {
   location = "us-west4"
 }
 
+resource "google_storage_bucket_object" "transform_marketcap_zip" {
+  name   = "transform_marketcap/function-source.zip"
+  source = "./cloud_functions/transform_marketcap/function-source.zip"
+  bucket = google_storage_bucket.cloud_fns_bucket.name
+}
+
+resource "google_storage_bucket_object" "load_file_into_cloud_storage_zip" {
+  name   = "load_file_into_cloud_storage/function-source.zip"
+  source = "./cloud_functions/load_to_warehouse/function-source.zip"
+  bucket = google_storage_bucket.cloud_fns_bucket.name
+}
+
 import {
   to = google_cloudfunctions2_function.transform_marketcap_fn
   id = "projects/my-learning-de/locations/us-west4/functions/transform_marketcap"
 }
 
 resource "google_cloudfunctions2_function" "transform_marketcap_fn" {
-  name = "transform_marketcap"
+  name     = "transform_marketcap"
   location = "us-west4"
 
   build_config {
-    runtime = "python311"
+    runtime     = "python311"
     entry_point = "transform_html_into_csv"
     source {
-      bucket = google_storage_bucket.cloud_fns_bucket.name
+      storage_source {
+        bucket = google_storage_bucket.cloud_fns_bucket.name
+        object = google_storage_bucket_object.transform_marketcap_zip.name
+      }
     }
+  }
+
+  service_config {
+    timeout_seconds                  = 60
+    max_instance_count               = 2
+    min_instance_count               = 0
+    max_instance_request_concurrency = 1
+    available_memory                 = "256M"
+    service_account_email            = "823447068653-compute@developer.gserviceaccount.com"
+    service                          = "projects/my-learning-de/locations/us-west4/services/transform-marketcap"
+    ingress_settings                 = "ALLOW_ALL"
+    environment_variables            = {}
+    available_cpu                    = "0.1666"
+    all_traffic_on_latest_revision   = true
   }
 }
